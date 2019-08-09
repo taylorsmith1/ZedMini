4b0c9979b13ca649c6585cec99b513ec4236852f
Irisa Myint <irisa.t.myint@vanderbilt.edu>
2019-08-09 11:36:53 -0500

    Instructions Added

--- Zed/Assets/Scripts/Experiment.cs
+++ Zed/Assets/Scripts/Experiment.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc3f61e6e47a8562629f45e7effd22d4f33ff5462ce2e015bb2ff80d0142492e
-size 14087
+oid sha256:94d8ca47dc820590404b95465ec73ff4b2ad1c037ad050ff0fa6c2273e138991
+size 14081
